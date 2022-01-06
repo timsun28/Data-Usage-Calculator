58ec04d0ed49d3884c324bbe5b3a196b362158d8
Timo Frionnet <tfrionnet@gmail.com>
2022-01-06 14:09:13 +0100

    changed theme color to gray-900

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,7 +16,7 @@ class MyDocument extends Document {
             <link rel="manifest" href="/site.webmanifest"/>
             <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
             <meta name="msapplication-TileColor" content="#da532c"/>
-            <meta name="theme-color" content="#ffffff"/>
+            <meta name="theme-color" content="#111827"/>
         </Head>
         <body>
           <Main />
